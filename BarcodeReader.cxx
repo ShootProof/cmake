8d360e12b18b2f1c3129a790f0046982d4a80ca4
Jeremiah Towe <jeremiah@shootproof.com>
2020-08-27 15:07:13 +0000

    Fix whitespace issues

--- BarcodeReader.cxx
+++ BarcodeReader.cxx
@@ -10,23 +10,23 @@ using namespace std;
 #if defined(LINUX) || defined(MACOS)
 	#include <sys/time.h>
 
-int gettime() 
+int gettime()
 {
-	struct timeval time; 
+	struct timeval time;
 	gettimeofday(&time, NULL);
 	return (int)(time.tv_sec * 1000 * 1000 +  time.tv_usec) / 1000;
-}						
+}
 #else
-int gettime() 
+int gettime()
 {
 	return (int)(GetTickCount());
 }
 #endif
 
 char* read_file_text(const char* filename) {
-	FILE *fp = fopen(filename, "r"); 
-    size_t size;
-    char *text = NULL;
+	FILE *fp = fopen(filename, "r");
+	size_t size;
+	char *text = NULL;
 	if (fp)
 	{
 		fseek(fp, 0, SEEK_END);
@@ -40,22 +40,22 @@ char* read_file_text(const char* filename) {
 	rewind(fp);
 	text = (char *)calloc((size + 1), sizeof(char));
 	if (text == NULL) {fputs ("Memory error",stderr); return NULL;}
-	
+
 	char c;
 	char *tmp = text;
 	do {
-      c = fgetc (fp);
-      *tmp = c;
-	  tmp++;
-    } while (c != EOF);
+		c = fgetc (fp);
+		*tmp = c;
+		tmp++;
+	} while (c != EOF);
 	fclose (fp);
 	return text;
 }
 
 unsigned char * read_file_binary(const char* filename, int* out_size) {
-	FILE *fp = fopen(filename, "rb"); 
-    size_t size;
-    unsigned char *buffer = NULL;
+	FILE *fp = fopen(filename, "rb");
+	size_t size;
+	unsigned char *buffer = NULL;
 	if (fp)
 	{
 		fseek(fp, 0, SEEK_END);
@@ -77,7 +77,7 @@ unsigned char * read_file_binary(const char* filename, int* out_size) {
 	return buffer;
 }
 
-int barcode_decoding(const unsigned char* buffer, int size, int formats, int threadcount, char* license, char* config) 
+int barcode_decoding(const unsigned char* buffer, int size, int formats, int threadcount, char* license, char* config)
 {
 	std::thread::id thread_id = std::this_thread::get_id();
 
@@ -107,7 +107,7 @@ int barcode_decoding(const unsigned char* buffer, int size, int formats, int thr
 	int iRet = reader.DecodeFileInMemory(buffer, (int)size, "");
 	int endtime = gettime();
 	int timecost = endtime - starttime;
-		
+
 	// Output barcode result
 	if (iRet != DBR_OK && iRet != DBRERR_MAXICODE_LICENSE_INVALID && iRet != DBRERR_AZTEC_LICENSE_INVALID && iRet != DBRERR_LICENSE_EXPIRED && iRet != DBRERR_QR_LICENSE_INVALID && iRet != DBRERR_GS1_COMPOSITE_LICENSE_INVALID &&
 		iRet != DBRERR_1D_LICENSE_INVALID && iRet != DBRERR_PDF417_LICENSE_INVALID && iRet != DBRERR_DATAMATRIX_LICENSE_INVALID && iRet != DBRERR_GS1_DATABAR_LICENSE_INVALID && iRet != DBRERR_PATCHCODE_LICENSE_INVALID)
@@ -118,16 +118,16 @@ int barcode_decoding(const unsigned char* buffer, int size, int formats, int thr
 
 	TextResultArray *paryResult = NULL;
 	reader.GetAllTextResults(&paryResult);
-		
+
 	if (paryResult->resultsCount == 0)
 	{
 		printf("No barcode found.\n");
 		CBarcodeReader::FreeTextResults(&paryResult);
 		return -1;
 	}
-	
+
 	printf("Thread id: %d. Total barcode(s) found: %d. Time cost: %d ms\n\n", thread_id, paryResult->resultsCount, timecost);
-	
+
 	// for (int index = 0; index < paryResult->resultsCount; index++)
 	// {
 	// 	printf("Barcode %d:\n", index + 1);
@@ -156,23 +156,23 @@ void ToHexString(unsigned char* pSrc, int iLen, char* pDest)
 void multi_thread_performance(int processor_count, unsigned char *buffer, int size, int formats, char* license, char* config)
 {
 	int minimum_count = 1, minimum_timecost = 0;
-	for (int i = 0; i < processor_count; i++) 
+	for (int i = 0; i < processor_count; i++)
 	{
 		printf("Thread count: %d. ", i + 1);
 		int timecost = barcode_decoding(buffer, size, formats, i, license, config);
-		if (i == 0) 
+		if (i == 0)
 		{
-			minimum_count = 1; 
+			minimum_count = 1;
 			if (timecost > 0)
 			{
-				minimum_timecost = timecost; 
+				minimum_timecost = timecost;
 			}
 		}
 		else {
 			if (timecost < minimum_timecost)
 			{
 				minimum_count = i + 1;
-				minimum_timecost = timecost; 
+				minimum_timecost = timecost;
 			}
 		}
 	}
@@ -188,7 +188,7 @@ int main(int argc, const char* argv[])
 
 	if (argc < 2) {
 		printf("Usage: BarcodeReader [image-file] [optional: license-file] [optional: template-file] \n");
-        return 0;
+		return 0;
 	}
 
 	char* license = NULL;
@@ -220,7 +220,7 @@ int main(int argc, const char* argv[])
 	// Call decoding methods on worker threads
 	printf("---------------- Decoding barcodes on worker threads ----------------\n\n");
 	int starttime = gettime();
-	// thread t1(barcode_decoding, buffer, size, BF_ONED); 
+	// thread t1(barcode_decoding, buffer, size, BF_ONED);
 	thread t2(barcode_decoding, buffer, size, BF_QR_CODE, 1, license, config);
 	thread t3(barcode_decoding, buffer, size, BF_PDF417, 1, license, config);
 	thread t4(barcode_decoding, buffer, size, BF_DATAMATRIX, 1, license, config);
@@ -241,7 +241,7 @@ int main(int argc, const char* argv[])
 	// All
 	printf("-------------------------------- All --------------------------------\n\n");
 	multi_thread_performance((int)processor_count, buffer, size, BF_ALL, license, config);
-	
+
 	free(license);
 	free(config);
 	free(buffer);
